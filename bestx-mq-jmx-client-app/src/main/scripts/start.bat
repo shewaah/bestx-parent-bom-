c03c2faa1b74cad3c1f0250f2b9b96b29243f386
robert.pinilla <robertgonzalez.pinilla@softsolutions.it>
2019-12-13 11:32:43 +0100

    BESTX-602: Add InstrumentName to Price Discovery Response
    * Fixed Start Script

--- bestx-mq-jmx-client-app/src/main/scripts/start.bat
+++ bestx-mq-jmx-client-app/src/main/scripts/start.bat
@@ -1,9 +1,9 @@
 @echo off
 rem
-rem This script starts the Grdlite Simulator application.
+rem This script starts the BestX MQ JMX Application.
 rem
-rem artifact ref.: bestx-grdlite-simulator
-rem script version 1.0 - 2016/03/10
+rem artifact ref.: bestx-mq-jmx-client-app
+rem script version 1.0 - 2019/12/13
 rem
 rem NOTICE: All information contained herein is, and remains the property of SoftSolutions! srl
 rem The intellectual and technical concepts contained herein are proprietary to SoftSolutions! srl and
@@ -17,11 +17,11 @@ rem
 rem Scripts vars
 
 SET JVM_HOME=C:\Program Files\Java\jdk1.8.0_112
-SET CURRENT_DIR=C:/softsolutions/bestx/services/bestx-mq-jmx-client-app
+SET CURRENT_DIR=C:/softsolutions/bestx/tools/bestx-mq-jmx-client
 
 set OPT_1=-server -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods
 set OPT_2=-XX:PermSize=128m -XX:MaxPermSize=256m -XX:HeapDumpPath=%CURRENT_DIR%/log/dump -XX:+HeapDumpOnOutOfMemoryError -Dcom.ibm.mq.cfg.useIBMCipherMappings=false
-set OPT_3=-Dss.app.name=BogusGRDLite -cp %CURRENT_DIR%\config;%CURRENT_DIR%\lib\*;
+set OPT_3=-Dss.app.name=BogusGRDLite -cp %CURRENT_DIR%\cfg;%CURRENT_DIR%\lib\*;
 
 SET JAVA_OPTIONS= %OPT_1% %OPT_2% %OPT_3%
 SET START_CLASS=it.softsolutions.bestx.mqclient.MQtest
