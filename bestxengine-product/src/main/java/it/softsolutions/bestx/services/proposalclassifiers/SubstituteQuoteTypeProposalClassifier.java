a650f5ad0e7c27572f1de9c5c2bc1c00f6e37e5a
robert.pinilla <robertgonzalez.pinilla@softsolutions.it>
2021-06-17 14:58:39 +0200

    BESTX-679: Composite prices saved as CMPwq

---
+++ bestxengine-product/src/main/java/it/softsolutions/bestx/services/proposalclassifiers/SubstituteQuoteTypeProposalClassifier.java
@@ -0,0 +1,37 @@
+package it.softsolutions.bestx.services.proposalclassifiers;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import it.softsolutions.bestx.bestexec.ProposalClassifier;
+import it.softsolutions.bestx.model.Attempt;
+import it.softsolutions.bestx.model.ClassifiedBook;
+import it.softsolutions.bestx.model.ClassifiedProposal;
+import it.softsolutions.bestx.model.Order;
+import it.softsolutions.bestx.model.Proposal;
+import it.softsolutions.bestx.model.Rfq.OrderSide;
+import it.softsolutions.bestx.model.Venue;
+
+public class SubstituteQuoteTypeProposalClassifier extends BaseMarketMakerClassifier implements ProposalClassifier {
+
+	@Override
+	public ClassifiedProposal getClassifiedProposal(ClassifiedProposal proposal, Order order, List<Attempt> previousAttempts, Set<Venue> venues, ClassifiedBook book) {
+		if (this.isCompositePriceMarketMaker(proposal)) {
+			proposal.setType(Proposal.ProposalType.COMPOSITE);
+		}
+		return proposal;
+	}
+
+	@Override
+	public ClassifiedProposal getClassifiedProposal(ClassifiedProposal proposal, OrderSide orderSide, BigDecimal qty, Date futSettDate, List<Attempt> previousAttempts, Set<Venue> venues) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public ClassifiedProposal getClassifiedProposal(ClassifiedProposal proposal, Order order, List<Attempt> previousAttempts, Set<Venue> venues) {
+		throw new UnsupportedOperationException();
+	}	
+	
+}
