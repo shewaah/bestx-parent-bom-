2149267908bdcf03a3b832e9c2a2cd63c7a68d1d
anna.cochetti <anna.cochetti@softsolutions.it>
2021-09-07 11:48:16 +0200

    BESTX-867 BESTX-891

---
+++ bestxengine-common/src/main/java/it/softsolutions/bestx/connections/TradeStacConnectorMBean.java
@@ -0,0 +1,38 @@
+
+/*
+ * Copyright 1997-2017 SoftSolutions! srl 
+ * All Rights Reserved. 
+ * 
+ * NOTICE: All information contained herein is, and remains the property of SoftSolutions! srl 
+ * The intellectual and technical concepts contained herein are proprietary to SoftSolutions! srl and 
+ * may be covered by EU, U.S. and other Foreign Patents, patents in process, and 
+ * are protected by trade secret or copyright law. 
+ * Dissemination of this information or reproduction of this material is strictly forbidden 
+ * unless prior written permission is obtained from SoftSolutions! srl.
+ * Any additional licenses, terms and conditions, if any, are defined in file 'LICENSE.txt', 
+ * which may be part of this software package.
+ */
+
+package it.softsolutions.bestx.connections;
+
+/**
+ *
+ * Purpose: this class is mainly for ...  
+ *
+ * Project Name : bestx-marketaxess-market
+ * First created by: anna.cochetti
+ * Creation date: 11 set 2017
+ * 
+ **/
+
+public interface TradeStacConnectorMBean {
+
+	public boolean isAddIncludeDealers();
+	public void setAddIncludeDealers(boolean addBlockedDealers);
+	public boolean isAddBlockedDealers();
+	public void setAddBlockedDealers(boolean addBlockedDealers);
+	public int getBlockedDealersMaxNum();
+	public void setBlockedDealersMaxNum(int blockedDealersMaxNum);
+	public int getIncludeDealersMaxNum();
+	public void setIncludeDealersMaxNum(int includeDealersMaxNum);
+}
